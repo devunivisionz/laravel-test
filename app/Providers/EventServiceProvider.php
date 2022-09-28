03709a77bf948ecbf43d9a5ac9792b61f2fab986
devunivisionz <63139612+sagarparmar6527@users.noreply.github.com>
2022-09-28 16:38:29 +0530

    Add Notification

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,9 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Events\RemoveUserEvent;
+use Illuminate\Auth\Events\CreateUserEvent;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use App\Listeners\SendRemoveUserNotification;
+use App\Listeners\SendCreateUserNotification;
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -17,6 +22,12 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        RemoveUserEvent::class => [
+            SendRemoveUserNotification::class,
+        ],
+        CreateUserEvent::class => [
+            SendCreateUserNotification::class,
         ],
     ];
 
